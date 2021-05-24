3ccc4798d8ab76eb63108909dc95e9de049e1731
AMIEL NAVARRO <anavarro18@cnm.edu>
2021-05-24 15:20:44 -0600

    Add project files.

---
+++ Exercise 1 - Hello World!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_1___Hello_World_
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+           
+        }
+    }
+}
